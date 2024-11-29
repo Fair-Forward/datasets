55a99b72d7d3c02d039ec6a3b1cf92470fc4836c
jonas-nothnagel <jonas.nothnagel@gmail.com>
2024-11-29 11:43:30 +0100

    update readme

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 Welcome to our organization's data catalog. Below is a list of datasets that have been collected throughout our programme Fair Forward.
 
-| Dataset Name                                | Year    | Description                                | Link                                                                                        | Documentation                                                                | Use-Case                                                                 |
-| ------------------------------------------- | ------- | ------------------------------------------ | ------------------------------------------------------------------------------------------- | ---------------------------------------------------------------------------- | ------------------------------------------------------------------------ |
-| Crop Analyses: AI for Telangana Agriculture | 2022–24 | AI-based crop type map and replication kit | [Dataset](https://dataexplorer.ts.adex.org.in/dataset/1da21f2b-87f6-4641-81bd-ed6bcd461303) | [Documentation](datasets-documentation/telangana_crop_data_documentation.md) | [Use-Case One Pager](use-case-one-pager/telangana_crop_data_use_case.md) |
-
+| Dataset Name | Year | Description | Dataset Link | Documentation | Use-Case |
+|--------------|------|-------------|--------------|---------------|----------|
+| Crop Analyses: AI for Telangana Agriculture | 2022–2024  | AI-based crop type map and replication kit | [Dataset Link](https://dataexplorer.ts.adex.org.in/dataset/1da21f2b-87f6-4641-81bd-ed6bcd461303) | [Documentation](datasets-documentation/telangana_crop_data_documentation.md) | [Use-Case One Pager](use-case-one-pager/telangana_crop_data_use_case.md) |
--- docs/generate_markdown_table.py
+++ docs/generate_markdown_table.py
@@ -5,8 +5,8 @@ excel_file = "data_catalog.xlsx"
 df = pd.read_excel(excel_file)
 
 # Open the Markdown file for writing
-output_file = "index.md"
-with open(output_file, "w") as f:
+index_file = "index.md"
+with open(index_file, "w") as f:
     # Write the header for the Markdown file
     f.write("# Data Catalog\n\n")
     f.write("Welcome to our organization's data catalog. Below is a list of datasets that have been collected throughout our programme Fair Forward.\n\n")
@@ -19,5 +19,23 @@ with open(output_file, "w") as f:
     for _, row in df.iterrows():
         f.write(f"| {row['Dataset Name']} | {row['Year']} | {row['Description']} | ")
         f.write(f"[Dataset Link]({row['Link']}) | ")
-        f.write(f"[Documentatino]({row['Documentation']}) | ")
+        f.write(f"[Documentation]({row['Documentation']}) | ")
+        f.write(f"[Use-Case One Pager]({row['Use-Case']}) |\n")
+
+# Open the Markdown file for updating Readme
+index_file = "../README.md"
+with open(index_file, "w") as f:
+    # Write the header for the Markdown file
+    f.write("# Data Catalog\n\n")
+    f.write("Welcome to our organization's data catalog. Below is a list of datasets that have been collected throughout our programme Fair Forward.\n\n")
+
+    # Write the Markdown table header
+    f.write("| Dataset Name | Year | Description | Dataset Link | Documentation | Use-Case |\n")
+    f.write("|--------------|------|-------------|--------------|---------------|----------|\n")
+
+    # Write each row from the Excel file into the Markdown table
+    for _, row in df.iterrows():
+        f.write(f"| {row['Dataset Name']} | {row['Year']} | {row['Description']} | ")
+        f.write(f"[Dataset Link]({row['Link']}) | ")
+        f.write(f"[Documentation]({row['Documentation']}) | ")
         f.write(f"[Use-Case One Pager]({row['Use-Case']}) |\n")
